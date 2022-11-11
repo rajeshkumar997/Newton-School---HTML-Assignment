076b95c427b19670a76fb13166b1fb45075d3546
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-11-11 23:09:20 +0530

    Delete JS Functions, JS for in, JS for of, JS String, Object.assign & Object.is, JS Rem ... - Post Class directory

--- JS Functions, JS for in, JS for of, JS String, Object.assign & Object.is, JS Rem ... - Post Class/JS String - 3.js
+++
@@ -1,5 +0,0 @@
-function doesItContain(str, substr) {
-    // write code here
-    // return the output , do not use console.log here
-    return str.includes(substr);
-}
--- JS Functions, JS for in, JS for of, JS String, Object.assign & Object.is, JS Rem ... - Post Class/Js Array Unique Numbers.js
+++
@@ -1,8 +0,0 @@
-function uniqueNumber(arrOfNum) {
-    // write code here
-    // return the output , do not use console.log here
-
-    // return [...new Set(arrOfNum)];
-                //or
-    return Array.from(new Set(arrOfNum));
-}
