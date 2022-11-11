e1b87b91c42c6e85c620e340d027672a94dc5d59
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-11-11 23:08:55 +0530

    Delete JS Bitwise operators, JS infinite loops, JS for in, JS for of, JS break and cont ... - In Class directory

--- JS Bitwise operators, JS infinite loops, JS for in, JS for of, JS break and cont ... - In Class/JS Object - 1.js
+++
@@ -1,9 +0,0 @@
-function createUserObj(email, pass) {
-    // write code here
-    // return the output, do not console.log it
-    let result = {
-        email: email,
-        password: pass
-    }
-    return result;
-}
